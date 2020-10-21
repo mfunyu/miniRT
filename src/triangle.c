c02f95897738f7b2c9231d09fb5e40fef0b6d53a
mfunyu <mfunyu@student.42tokyo.jp>
2020-10-21 22:02:10 +0900

    save

--- src/triangle.c
+++ src/triangle.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/04 21:05:07 by mfunyu            #+#    #+#             */
-/*   Updated: 2020/10/20 23:10:36 by mfunyu           ###   ########.fr       */
+/*   Updated: 2020/10/21 22:01:45 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,51 +23,46 @@ void	set_triangle_normal(t_tr tr, double normal[3])
 	normalize(normal);
 }
 
-bool	is_intersect_inside_tr(t_tr tr, double p[3], double normal[3])
-{
-	double	ab[3];
-	double	bc[3];
-	double	ca[3];
-	double	coord_to_p[3];
-	double	check[3];
-
-	vec_sub(coord_to_p, tr.coord1, p);
-	cross_p(check, ab, coord_to_p);
-	if (dot_p(check, normal) < 0)
-		return (false);
-	vec_sub(bc, tr.coord2, tr.coord3);
-	vec_sub(coord_to_p, tr.coord2, p);
-	cross_p(check, bc, coord_to_p);
-	if (dot_p(check, normal) < 0)
-		return (false);
-	vec_sub(ca, tr.coord3, tr.coord1);
-	vec_sub(coord_to_p, tr.coord3, p);
-	cross_p(check, ca, coord_to_p);
-	if (dot_p(check, normal) < 0)
-		return (false);
-	return (true);
-}
-
 double	calc_triangle(double dir[3], double orig[3], t_tr tr)
 {
+	double	ab[3];
+	double	ac[3];
 	double	normal[3];
-	double	p[3];
 	double	t;
-	int		k;
 
-	set_triangle_normal(tr, normal);
+	vec_sub(ab, tr.coord1, tr.coord2);
+	vec_sub(ac, tr.coord1, tr.coord3);
+	cross_p(normal, ab, ac);
+	normalize(normal);
 	t = calc_plane(orig, tr.coord1, normal, dir);
 	if (t < EPSILON)
 		return (-1);
-	k = 0;
+	int k = 0;
+	double p[3];
 	while (k < 3)
 	{
 		p[k] = orig[k] + t * dir[k];
 		k++;
 	}
-	if (is_intersect_inside_tr(tr, p, normal))
-		return (t);
-	return (-1);
+	double bc[3];
+	double ca[3];
+	double vp[3];
+	double test[3];
+	vec_sub(vp, tr.coord1, p);
+	cross_p(test, ab, vp);
+	if (dot_p(test, normal) < 0)
+		return (-1);
+	vec_sub(bc, tr.coord2, tr.coord3);
+	vec_sub(vp, tr.coord2, p);
+	cross_p(test, bc, vp);
+	if (dot_p(test, normal) < 0)
+		return (-1);
+	vec_sub(ca, tr.coord3, tr.coord1);
+	vec_sub(vp, tr.coord3, p);
+	cross_p(test, ca, vp);
+	if (dot_p(test, normal) < 0)
+		return (-1);
+	return (t);
 }
 
 void	set_triangle(t_c camera, t_info *info, t_elem *elem, int index)
@@ -82,7 +77,7 @@ void	set_triangle(t_c camera, t_info *info, t_elem *elem, int index)
 	info->t = t;
 	set_vec(info->rgb, elem->tr[index].rgb);
 	set_pvec(info->p, camera.coord, info->dc, t);
-	set_triangle_normal(elem->tr[index], info->p);
+	set_triangle_normal(elem->tr[index], info->n);
 	adjust_normal_vec(info->n, camera.coord, info->p);
 }
 
